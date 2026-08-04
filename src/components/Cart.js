3dc22e089ab7b6efc39e2f506748c314b709476a
ahlife18 <ahlife18@gmail.com>
2026-08-04 12:50:57 +0100

    Add files via upload

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -31,7 +31,7 @@ function Cart({ cart, removeFromCart, clearCart }) {
     try {
       // 🔥 PAYSTACK SETUP WITH YOUR EMAIL
       const handler = window.PaystackPop.setup({
-        key: 'pk_test_ac9fa0f6232af51eedd512720eb32f8dc008289a', // ✅ Your Live Public Key
+        key:'pk_live_827aae9b1ef3daa5bec39d6a04107e7', // ✅ Your Live Public Key
         email: 'kolawoleemanuel@gmail.com',              // ✅ Your email (where money alerts go)
         amount: Math.round(total * 100),                 // ✅ Converts dollars to cents
         currency: 'NGN',                                 // ✅ Change to 'NGN' if you want Naira
