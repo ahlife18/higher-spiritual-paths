c3630b7f3c9a12bd9abf100e93acd1f0e3c28dbd
ahlife18 <ahlife18@gmail.com>
2026-08-12 11:09:05 -0400

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -167,8 +167,8 @@ function Home() {
         <img src="/practice.png" alt="Community" style={styles.footerImg} />
         <div style={styles.contactInfo}>
           <div>📞 Bennie Hollis: +1 (352) 544-9302</div>
-          <div>📞 Karen Johnson: +1 (713) 384-0334</div>
-          <div>📞 Kolawole Emmanuel: +234 (081) 08491093</div>
+          <div>📞 Karen Johnson: +1 (713) 384-0344</div>
+          <div>📞 Kolawole Emmanuel: +234 (081) 084-91093</div>
         </div>
         <div style={styles.footerDivider}></div>
         <div style={styles.footer}>THANK YOU! ❤️</div>
